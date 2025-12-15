bf5df804d19832d4ccd87bc26b26bc926a8501ec
Shwetha2810 <shwethamogaveera01@gmail.com>
2025-12-15 14:14:08 +0530

    aded sighup

--- src/main/java/com/chitchat/controller/HomeController.java
+++ src/main/java/com/chitchat/controller/HomeController.java
@@ -3,7 +3,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
- @GetMapping("/") public String root(){ return "login"; }
- @GetMapping("/home") public String home(){ return "home"; }
- @GetMapping("/signup") public String signup(){ return "signup"; }
+
+    @GetMapping("/")
+    public String root() {
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
+
