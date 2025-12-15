5594af57582c50da3dfc2c9a3e0d027ef1a453a0
Shwetha2810 <shwethamogaveera01@gmail.com>
2025-12-15 13:04:58 +0530

    first commit

---
+++ src/main/java/com/chitchat/ChitChatApplication.java
@@ -0,0 +1,9 @@
+package com.chitchat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ChitChatApplication {
+ public static void main(String[] args) {
+  SpringApplication.run(ChitChatApplication.class, args);
+ }
+}
